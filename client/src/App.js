b116571addae9ffe5487a01bcc5c32f67ba6a186
hoojianliang <hoojianliang@users.noreply.github.com>
2022-01-15 20:06:37 +0800

    implement mui theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,106 @@
-import logo from "./logo.svg";
 import "./App.scss";
+import * as React from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
+import Palette from './palette';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./routes/main";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { amber, deepOrange, grey } from '@mui/material/colors';
 
-function App() {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+const getDesignTokens = (mode) => ({
+  palette: {
+    mode,
+    primary: {
+      ...amber,
+      ...(mode === 'dark' && {
+        main: amber[300],
+      }),
+    },
+    ...(mode === 'dark' && {
+      background: {
+        default: deepOrange[900],
+        paper: deepOrange[900],
+      },
+    }),
+    text: {
+      ...(mode === 'light'
+        ? {
+            primary: deepOrange[900],
+            secondary: grey[800],
+          }
+        : {
+            primary: '#fff',
+            secondary: grey[500],
+          }),
+    },
+  },
+});
+
+export default function ToggleColorMode() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
-export default App;
+function App() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <div className="App">
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            bgcolor: 'background.default',
+            color: 'text.primary',
+            borderRadius: 1,
+            p: 3,
+          }}
+        >
+          {theme.palette.mode} mode
+          <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+            {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </Box>
+        <Demo />
+        <Palette />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="expenses" element={<Expenses />} />
+            <Route path="invoices" element={<Invoices />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </StyledEngineProvider>
+  );
+}
---
+++ client/src/palette.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function Palette() {
+  return (
+    <>
+      <Button>Primary</Button>
+      <Button color="secondary">Secondary</Button>
+    </>
+  );
+}
